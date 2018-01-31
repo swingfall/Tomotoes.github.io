551505cf08bd218fa08df26856b99642c42f7706
JinmaQAQ <jinmaup@gmail.com>
2018-01-31 14:51:54 +0800

    x

--- js/site.js
+++ js/site.js
@@ -340,8 +340,6 @@ elf(function () {
 	}
 	site.Translation.translate(navigator.language || "zh-CN");
 
-	elf("a.mail").attr("href", elf().template("mailto:#{0}@#{1}", "jinmaup", "gmail.com"));
-
 	let module = document.body.className.replace(/page-type-/g, "").split(" ");
 	module.forEach(function (item) {
 		let initer = site.InitMap[item];
