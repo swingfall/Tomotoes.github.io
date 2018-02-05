77b9eb24c54b71488651c27a960ae8840d291098
JinmaQAQ <jinmaup@gmail.com>
2018-02-05 22:57:14 +0800

    I

--- source/site.js
+++ source/site.js
@@ -77,7 +77,7 @@ function typetitle(){
 		if(_i <= typingbefore.length){
 			title.innerHTML = `${typingbefore.slice(0,_i++)}|`;//将标题内容通过slice()方法返回
 			
-			setTimeout(typetitle,100);//每100毫秒执行一次
+			setTimeout(typetitle,180);//每100毫秒执行一次
 		}else{
 			title.innerHTML = typingbefore;//当标题内容全部返回后，去掉最后的‘|’
 		}
